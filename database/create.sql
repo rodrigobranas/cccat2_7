7e751ba3e20ef6447f2c4d23fb087dfd3d9c2845
Rodrigo Branas <rodrigo.branas@gmail.com>
2021-08-11 08:59:38 -0300

    updated

--- database/create.sql
+++ database/create.sql
@@ -1,11 +1,15 @@
 create schema ccca;
 
 create table ccca.item (
-    id serial,
-    description text,
-    price numeric,
-    height integer,
-    width integer,
-    length integer,
-    weight integer
+	id serial primary key,
+	description text,
+	price numeric,
+	width integer,
+	height integer,
+	length integer,
+	weight integer
 );
+
+insert into ccca.item (description, price, width, height, length, weight) values ('Guitarra', 1000, 100, 50, 15, 3);
+insert into ccca.item (description, price, width, height, length, weight) values ('Amplificador', 5000, 50, 50, 50, 22);
+insert into ccca.item (description, price, width, height, length, weight) values ('Cabo', 30, 10, 10, 10, 1);
